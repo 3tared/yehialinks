818e33b8d3d36ff868621fa137a28246ca422873
3tared <mohamed2011305977@gmail.com>
2026-01-31 00:05:45 +0200

    update image

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -157,7 +157,7 @@ export default function ProfessionalLinktree() {
                   <img
                     src={OWNER_DATA.avatar}
                     alt={OWNER_DATA.name}
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-center"
                   />
                 </div>
               </div>
